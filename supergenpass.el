8b76ecee5bd392e29e2f22210bf09853e4c8417e
Jaime Fournier <jaimef@linbsd.org>
2013-02-27 14:19:46 -0800

    Cleanup based on feedback from #emacs

--- supergenpass.el
+++ supergenpass.el
@@ -38,56 +38,49 @@
 ;;   :group 'sgp
 ;;   :type 'data)
 
-(defun b64_md5 (pickle)
+(defun b64-md5 (pickle)
   "Encrypt the string given to us as Base64 encoded Md5 byte stream"
   (replace-regexp-in-string "=" "A" (replace-regexp-in-string "+" "9" (replace-regexp-in-string "/" "8" (base64-encode-string (secure-hash 'md5 pickle nil nil t))))))
 
-(defun sgp_prompt_pass () (interactive)
+(defun sgp-prompt-pass () (interactive)
   "Interactive function to prompt for domain and password and populate paste buffer with new password"
   (setq domain (read-from-minibuffer "SuperGenPass: Domain? "))
   (setq password (read-passwd "SuperGenPass: Password? "))
-  (kill-new (format "%s" (sgp_generate password domain))))
+  (kill-new (format "%s" (sgp-generate password domain))))
 
 (defun supergenpass (password domain)
-  (kill-new (format "%s" (sgp_generate password domain))))
+  (kill-new (format "%s" (sgp-generate password domain))))
 
-(defun sgp_generate (password domain)
+(defun sgp-generate (password domain)
   "Create a unique password for a given domain and master password"
-  (setq i 0)
+  (let ((i 0) (results (format "%s:%s" password domain)))
   (setq results (format "%s:%s" password domain))
-
   (loop until
         (and (> i 10) (secure-enough results 10))
         do
-        (setq results (b64_md5 results))
-        (setq i (+ i 1))
+        (setq results (b64-md5 results))
+        (setq i (1+ i))
         )
-  (substring results 0 10))
+  (substring results 0 10)))
 
 (defun secure-enough (results len)
   "Ensure the password we have is sufficiently secure"
   (let
       ((case-fold-search nil))
-    (if
         (and
-         (> (length results) 10)
-         (string-match "[0-9]" (substring results 0 10))
-         (string-match "[A-Z]" (substring results 0 10))
-         (string-match "^[a-z]" (substring results 0 10)))
-        t
-      nil
-      )))
+         (> (length results) len)
+         (string-match "[0-9]" (substring results 0 len))
+         (string-match "[A-Z]" (substring results 0 len))
+         (string-match "^[a-z]" (substring results 0 len)))))
 
-;; (defun sgp_test_compat () (interactive)
-;;   (if
-;;       (and
-;;        (equal "ubRPZrdfu5" (sgp_generate "abcdefg" "cnn.com"))
-;;        (equal "xaIlcL02A3" (sgp_generate "TheSeasonedSchemer" "slashdot.org"))
-;;        (equal "oZ8YbHxSKg" (sgp_generate "LiveLongAndProper" "lisp.org"))
-;;        )
-;;       (message "All Passed")
-;;     (message "Failures!")
-;;     ))
+(defun sgp-test-compat () (interactive)
+  (if
+      (and
+       (equal "ubRPZrdfu5" (sgp-generate "abcdefg" "cnn.com"))
+       (equal "xaIlcL02A3" (sgp-generate "TheSeasonedSchemer" "slashdot.org"))
+       (equal "oZ8YbHxSKg" (sgp-generate "LiveLongAndProper" "lisp.org")))
+      (message "All Passed")
+    (message "Failures!")))
 
 (provide 'supergenpass)
 
