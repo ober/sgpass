dd20916eeec890abbf7a113deb5931a956465369
Jaime Fournier <jaimef@linbsd.org>
2013-02-27 01:35:42 -0800

    Add Tests to help find the bug I introduced with compat with other supergenpass programs.

--- supergenpass.el
+++ supergenpass.el
@@ -25,7 +25,7 @@
 ;; Based heavily off the Ruby implementation by rstacruz
 ;; https://github.com/rstacruz/sgpass.git
 ;;
-;;
+
 ;; Many thanks to #emacs on Freenode for assistance.
 
 ;;; Code:
@@ -54,23 +54,45 @@
 (defun sgp_generate (password domain)
   "Create a unique password for a given domain and master password"
   (setq i 0)
-  (setq result (format "%s:%s" password domain))
+  (setq results (format "%s:%s" password domain))
+
   (loop until
-        (and (> i 10) (secure-enough result 10))
+        (and (> i 10) (secure-enough results 10))
         do
-        (setq result (b64_md5 result))
+        (message "%s %s %s" i results (secure-enough results 10))
+        (setq results (b64_md5 results))
         (setq i (+ i 1))
         )
-  (substring result 0 10))
+  (message "%s %s %s" i results (secure-enough results 10))
+  (substring results 0 10))
 
 (defun secure-enough (results len)
   "Ensure the password we have is sufficiently secure"
-  (if
-      (and (> (length results) len) (let ((case-fold-search nil)) (string-match "[0-9]" (substring results 0 10)) (string-match "[A-Z]" (substring results 0 10)) (string-match "^[a-z]" (substring results 0 10))))
-      t
-    nil
-    ))
+  (let
+      ((case-fold-search nil))
+    (if
+        (and
+         (> (length results) 10)
+         (string-match "[0-9]" (substring results 0 10))
+         (string-match "[A-Z]" (substring results 0 10))
+         (string-match "^[a-z]" (substring results 0 10)))
+        t
+      nil
+      )))
+
+(defun sgp_test_compat () (interactive)
+  (setq test1
+        (if
+            (and
+             (equal "ubRPZrdfu5" (sgp_generate "abcdefg" "cnn.com"))
+             (equal "xaIlcL02A3" (sgp_generate "TheSeasonedSchemer" "slashdot.org"))
+             )
+            (message "All Passed")
+          (message "Failures!")
+          )))
+
 
 (provide 'supergenpass)
+(substring "abcdefg
 
 ;; supergenpass.el ends here
